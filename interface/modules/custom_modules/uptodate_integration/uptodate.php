581d084bc58de3192c4d9fca9e16a660d2e70865
ericsbelcher <63966336+ericsbelcher@users.noreply.github.com>
2025-02-15 07:25:08 -0500

    Create uptodate.php

---
+++ interface/modules/custom_modules/uptodate_integration/uptodate.php
@@ -0,0 +1,8 @@
+<html>
+<head>
+    <title>UpToDate</title>
+</head>
+<body>
+    <iframe src="https://www.uptodate.com/login" width="100%" height="600px" style="border: none;"></iframe>
+</body>
+</html>
