050571144463680600b6ca066c2de3230535128a
ericsbelcher <63966336+ericsbelcher@users.noreply.github.com>
2025-02-15 07:26:21 -0500

    Create openevidence.php

---
+++ interface/modules/custom_modules/openevidence_integration/openevidence.php
@@ -0,0 +1,8 @@
+<html>
+<head>
+    <title>Open Evidence</title>
+</head>
+<body>
+    <iframe src="https://www.openevidence.com/" width="100%" height="600px" style="border: none;"></iframe>
+</body>
+</html>
